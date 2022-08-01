d051cc83eac02a6c6d615ea4320936db41d2edfc
DiegoTaborda <diegotabordapereira@gmail.com>
2022-08-01 15:44:24 -0300

    primeiro commit

---
+++ api.js
@@ -0,0 +1,164 @@
+const db = require('./db');
+const {
+    GetItemCommand,
+    PutItemCommand,
+    DeleteItemCommand,
+    ScanCommand,
+    UpdateItemCommand,
+} = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
+
+const getPaciente = async (event) => {
+    const response = { statusCode: 200 };
+
+    try {
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Key: marshall({ pacienteId: event.pathParameters.postId }),
+        };
+        const { Item } = await db.send(new GetItemCommand(params));
+
+        console.log({ Item });
+        response.body = JSON.stringify({
+            message: "Successfully retrived paciente.",
+            data: (Item) ? unmarshall(Item) : {},
+            rawData: Item,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to get paciente",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+};
+
+const createPaciente = async (event) => {
+    const response = { statusCode: 200 };
+
+    try {
+        const body = JSON.parse(event.body);
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Item: marshall(body || {}),
+        };
+        const createResult = await db.send(new PutItemCommand(params));
+
+        response.body = JSON.stringify({
+            message: "Successfully created paciente.",
+            createResult,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to create paciente",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+}; 
+
+const updatePaciente = async (event) => {
+    const response = { statusCode: 200 };
+
+    try {
+        const body = JSON.parse(event.body);
+        const objKeys = Object.keys(body);
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Key: marshall({ postId: event.pathParameters.postId }),
+            UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
+            ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
+                ...acc,
+                [`#key${index}`]: key,
+            }), {}),
+            ExpressionAttributeValues: marshall(objKeys.reduce((acc, key, index) => ({
+                ...acc,
+                [`:value${index}`]: body[key],
+            }), {})),
+        };
+        const updateResult = await db.send(new UpdateItemCommand(params));
+
+        response.body = JSON.stringify({
+            message: "Successfully updated paciente.",
+            createResult,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to update paciente",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+};
+
+const deletePaciente = async (event) => {
+    const response = { statusCode: 200 };
+
+    try {
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Key: marshall({ postId: event.pathParameters.postId }),
+            
+        };
+        const deleteResult = await db.send(new DeleteItemCommand(params));
+
+        response.body = JSON.stringify({
+            message: "Successfully deleted paciente.",
+            createResult,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to delete paciente",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+};
+
+const getAllPacientes = async (event) => {
+    const response = { statusCode: 200 };
+
+    try {
+        const { Items } = await db.send(new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME }));
+
+        response.body = JSON.stringify({
+            message: "Successfully retrieved all paciente.",
+            data: Items.map(( item ) => unmarshall(item)),
+            Items,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to retrieved paciente",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+};
+
+modules.exports = {
+    getPaciente,
+    createPaciente,
+    updatePaciente,
+    deletePaciente,
+    getAllPacientes,
+};
